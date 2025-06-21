90594d6e819045673c3433a10e05dce04ad17643
Lily4u <usmannadeem015@gmail.com>
2025-06-21 15:12:38 +0500

    Added basic JS

---
+++ JS/script.js
@@ -0,0 +1,55 @@
+// Footer date fetch 
+$(document).ready(function() {
+  $("#current-year").text(new Date().getFullYear());
+});
+
+// Recipie Finder logic
+$(document).ready(function () {
+// Recipes
+  const recipes = [
+  {
+    name: "Anda Biryani",
+    ingredients: ["rice", "egg", "onion"],
+  },
+  {
+    name: "Daal Tarka",
+    ingredients: ["daal", "onion", "tomato"],
+  },
+  {
+    name: "Aloo Bhujia",
+    ingredients: ["potato", "oil", "salt"],
+  },
+  {
+    name: "Egg Toast",
+    ingredients: ["bread", "egg", "salt"],
+  },
+];
+// Recipes end
+
+$("#ingredient-form").submit(function(e){
+  // Prevent page from reloading when button is clicked
+  e.preventDefault();
+
+  // .val() gets input, .toLowerCase() turns input to lower case, trim() removes spaces
+  const ing1= $("#ing1").val().toLowerCase().trim();
+  const ing2= $("#ing2").val().toLowerCase().trim();
+  const ing3= $("#ing3").val().toLowerCase().trim();
+
+  // Filter Empty inputs 
+  const userIngredients = [ing1, ing2, ing3].filter(Boolean);
+  // Result Div 
+  const resultDiv = $('#recipe-result');
+
+  // Make sure user enters 1 ing 
+  if (userIngredients.length === 0) {
+      $('#recipe-result').html("⚠️ Please enter at least one ingredient.");
+      return;
+    }
+
+
+})
+
+});
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Techwiz2025
+Adds all new and modified files
+git add .
+git commit -m "Any text here"
+
+git push origin master
